7700d4079219c75b9ae1c4ba75862a83f397dd1a
Payal-Raj <piyushikumari35@gmail.com>
2026-02-27 08:25:00 +0500

    Setup routes for Navlinks

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import './Sidebar.css'
 import { MdAddCircleOutline, MdListAlt, MdShoppingCart } from "react-icons/md";
+import { NavLink } from 'react-router-dom';
 
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-        <div className="sidebar-option">
+        <NavLink to={'/add'} className="sidebar-option">
             <MdAddCircleOutline className="icon" />
             <p>Add Items</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to={'/list'} className="sidebar-option">
             <MdListAlt className="icon" />
             <p>List Items</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to={'/orders'} className="sidebar-option">
             <MdShoppingCart className="icon" />
             <p>Orders</p>
-        </div>
+        </NavLink>
       
     </div>
   )
