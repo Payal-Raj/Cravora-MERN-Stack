5bd45284ddd333d7dd3867f25bf5fa79f8d2b12f
Payal-Raj <piyushikumari35@gmail.com>
2026-03-01 16:15:14 +0500

    feat: add JWT token generation on user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,6 +8,10 @@ const loginUser = async(req, res) => {
 
 }
 
+const createToken = (id) => {
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
+
 // register new user
 const registerUser = async (req, res) =>  {
     const {name, password, email} = req.body;
@@ -24,26 +28,30 @@ const registerUser = async (req, res) =>  {
             
         }
 
-         if (password.length < 8) {
+        if (password.length < 8) {
             return res.json({success:false, message: "Please enter a strong password."})
             
-         }
+        }
 
          //hashing user password
-         const salt = await bcrypt.genSalt(10)
-         const hashedPassword = await bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-         const newUser = new userModel({
+        const newUser = new userModel({
             name: name,
             email: email,
             password: hashedPassword
-         });
+        });
 
-         const user = await newUser.save();
+        const user = await newUser.save();
+
+        const token = createToken(user._id);
+
+        res.json({success:true, token})
     } catch (error) {
-        
+        console.log(error);
+        res.json({success:false, message:"Error"});
     }
-    
 }
 
 export {loginUser, registerUser};
