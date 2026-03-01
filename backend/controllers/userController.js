49ed65309c5679f9bcb82f9047dadf2d04702f05
Payal-Raj <piyushikumari35@gmail.com>
2026-03-01 16:51:50 +0500

    API testing and fixed typo error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,7 @@ const loginUser = async(req, res) => {
 }
 
 const createToken = (id) => {
-    return jwt.sign({id},process.env.JWT_SECRET)
+    return JWT.sign({id},process.env.JWT_SECRET)
 }
 
 // register new user
