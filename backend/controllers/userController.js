b737aa50a44fb514ea4649411b61d8bf11f6cc96
Payal-Raj <piyushikumari35@gmail.com>
2026-03-01 09:49:21 +0500

    Add user authentication boilerplate structure.

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+import userModel from "../models/userModels.js";
+import JWT from 'jsonwebtoken'
+import bcrypt from 'bcrypt';
+import validator from 'validator'
+
+//login user
+const loginUser = async(req, res) => {
+
+}
+
+// register new user
+const registerUser = async (req, res) =>  {
+    
+}
+
+export {loginUser, registerUser};
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ connectDB();
 //appendpoints A specific URL where your API receives or sends data.
 app.use("/api/food",foodRouter)
 app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
 
 app.get("/",(req, res) =>{
     res.send("API WORKING")
