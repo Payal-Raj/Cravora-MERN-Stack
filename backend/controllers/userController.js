3838433735480e7fe4edc77f0d54b3d12f78484d
Payal-Raj <piyushikumari35@gmail.com>
2026-03-01 09:54:26 +0500

    Add user registration logic with email existence check.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,6 +10,16 @@ const loginUser = async(req, res) => {
 
 // register new user
 const registerUser = async (req, res) =>  {
+    const {name, password, email} = req.body;
+    try {
+        //is user already exist.
+        const exist = await userModel.findOne({email});
+        if (exist) {
+            return res.json({success:false, message: "User already exists."})
+        }
+    } catch (error) {
+        
+    }
     
 }
 
